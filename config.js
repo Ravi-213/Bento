621d74cbcda7e87b8fb8dc8e11a96967686842e8
Ravi-213 <114809358+Ravi-213@users.noreply.github.com>
2022-10-01 14:08:21 +0200

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ const CONFIG = {
 
 	// Greetings
 	greetingMorning: 'Good morning!',
-	greetingAfternoon: 'Good afternoon,''!',
+	greetingAfternoon: 'Good afternoon,',
 	greetingEvening: 'Good evening,',
 	greetingNight: 'Go to Sleep!',
 
@@ -53,9 +53,9 @@ const CONFIG = {
 	firstButtonsContainer: [
 		{
 			id: '1',
-			name: 'Github',
-			icon: 'github',
-			link: 'https://github.com/',
+			name: 'Youtube',
+			icon: 'youtube',
+			link: 'https://youtube.com/',
 		},
 		{
 			id: '2',
@@ -83,9 +83,9 @@ const CONFIG = {
 		},
 		{
 			id: '6',
-			name: 'Odysee',
-			icon: 'youtube',
-			link: 'https://odysee.com/',
+			name: 'Whatsapp',
+			icon: 'whatsapp',
+			link: 'https://web.whatsapp.com/',
 		},
 	],
 
